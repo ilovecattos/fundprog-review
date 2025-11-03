4b06d91946dc3b1282c579a940c5b591b23d00f3
LSTR-512 <136193132+ilovecattos@users.noreply.github.com>
2025-11-03 20:12:18 +0800

    Add files via upload

---
+++ 67.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std; 
+
+int main ()
+{
+	int a,b; 
+	a = 5; 
+	b = a++ + 2; 
+	cout << "\nA: " <<a; 
+	cout << "\nB: " <<b;
+	
+	return 0; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
